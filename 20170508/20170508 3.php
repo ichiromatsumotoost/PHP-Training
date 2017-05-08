c0e24c2413ff660812c2a4f3a775245cd9b22177
ichiromatsumotoost <matuichi160@gmail.com>
2017-05-08 17:17:09 +0900

    変数

---
+++ 20170508/20170508 3.php
@@ -0,0 +1,9 @@
+<?php
+function mytest(){
+   $x=22;//僕の年齢
+   echo $x;
+}
+
+mytest();
+
+?>
---
+++ 20170508/20170508 4.php
@@ -0,0 +1,11 @@
+<?php
+$x=5;//yey
+
+function hey(){//1
+    global $x;
+
+    echo $x;//ye
+}//1
+
+hey();
+ ?>
